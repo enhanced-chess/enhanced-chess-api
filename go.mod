d3a243295818bb82780b52b3c217368853b3ef5a
Aaron Merrill <aaron@merrills.website>
2021-09-23 12:37:25 -0500

    Created dependency on rpio. First successful build

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/enhanced-chess/enhanced-chess-api
+
+go 1.17
+
+require github.com/stianeikeland/go-rpio v4.2.0+incompatible // indirect
--- src/main.go
+++ src/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+
+	rpio "github.com/stianeikeland/go-rpio"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+	fmt.Println("opening gpio")
+	err := rpio.Open()
+	if err != nil {
+		panic(fmt.Sprint("unable to open gpio", err.Error()))
+	}
+
+	defer rpio.Close()
+
+	pin := rpio.Pin(18)
+	pin.Output()
+	pin.Toggle()
+	time.Sleep(time.Second / 5)
+	pin.Toggle()
 }
