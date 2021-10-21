5769ffc59a79e03b763efb3370ce32b377989b80
Aaron Merrill <aaron@merrills.website>
2021-10-21 12:24:22 -0500

    Create method that will eventually do the logic translating from coordinates to an int for the pi

--- src/main.go
+++ src/main.go
@@ -7,6 +7,11 @@ import (
 	rpio "github.com/stianeikeland/go-rpio"
 )
 
+func positionToInt(x int, y int) int {
+	//TODO implement translation from coordinates to binary for led array
+	return 16
+}
+
 func main() {
 	fmt.Println("opening gpio")
 	err := rpio.Open()
@@ -16,9 +21,18 @@ func main() {
 
 	defer rpio.Close()
 
+	// Test connection to gpio works
 	pin := rpio.Pin(18)
 	pin.Output()
 	pin.Toggle()
-	time.Sleep(time.Second / 5)
+	time.Sleep(time.Second / 1)
+	pin.Toggle()
+
+	// Test call to method works
+	intToSendToPi := positionToInt(0, 0)
+	pin = rpio.Pin(intToSendToPi)
+	pin.Output()
+	pin.Toggle()
+	time.Sleep(time.Second / 1)
 	pin.Toggle()
 }
